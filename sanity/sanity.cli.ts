3fa99b83ee2c020ff6ba30405881387550211cc5
jsh0116 <seunghoon7561@gmail.com>
2023-10-31 15:52:56 +0900

    [feat] init project app
    - create next app으로 프로젝트 생성
    - 필요 패키지 설치
    - tailwind 설정
    - sanity studio 설정

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '03v4bu4m',
+    dataset: 'production'
+  }
+})
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1 className='text-gray-400'>Instagram</h1>
+  );
+}
